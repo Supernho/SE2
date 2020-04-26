a588d750790b387de43a755949dcb609088180c0
Supernho <63387407+Supernho@users.noreply.github.com>
2020-04-26 21:38:54 +0700

    Update CountryService.java

--- src/Service/CountryService.java
+++ src/Service/CountryService.java
@@ -1,4 +1,20 @@
 package Service;
 
+import Model.Country;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface CountryService {
+	Country findCountryById(Long id);
+
+    List<Country> findAll();
+
+    void updateCountry(Country country);
+
+    void createCountry(Country country);
+
+    void deleteCountry(Long id);
+
 }
+
